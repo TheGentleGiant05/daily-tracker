bba548bbb46be5ae212e6613dffd18fca2821153
Jayson Hibbard <jhibbard055@gmail.com>
2021-06-03 00:51:38 -0600

    start
    change title, first component start, and firebase started up

--- daily-tracker/src/App.js
+++ daily-tracker/src/App.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import firebase from "firebase";
 
 const App = () => {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const firebaseApp = firebase.apps[0];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (item) {
+      const newItem = { id: new Date().getTime().toString(), item: item };
+      setItems((items) => {
+        return [...items, newItem];
+      });
+      setItem("");
+    } else {
+      window.alert("please insert an item");
+    }
+  };
+
   return (
     <div>
-      <h2>hello</h2>
+      <h1 className="title">Daily Tracker</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter new item here..."
+          id="item"
+          name="item"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button type="submit" value="Submit">
+          Add New
+        </button>
+      </form>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
+      {items.map((newItem) => {
+        const { id, item } = newItem;
+        return (
+          <div key={id}>
+            <h3>{item}</h3>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- daily-tracker/src/index.js
+++ daily-tracker/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import "./index.css";
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB6s3v9pfOCr_SdJ1_4mEKKpSK3Tu0p3ek",
+  authDomain: "daily-tracker-20dfe.firebaseapp.com",
+  projectId: "daily-tracker-20dfe",
+  storageBucket: "daily-tracker-20dfe.appspot.com",
+  messagingSenderId: "753304571819",
+  appId: "1:753304571819:web:0b2776902f65e119000cee",
+  measurementId: "G-3MM3VMP54V",
+});
 
 ReactDOM.render(
   <React.StrictMode>
